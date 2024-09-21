86daf48e154b54076ce5e7284d0d63c4d7304216
Oliver <oliver.azzolina@gmx.de>
2024-09-21 17:45:57 +0200

    added sidebar design and user data connection to firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,11 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"da-bubble-8cc28","appId":"1:380502943179:web:4f36bdcdc4230e89ccc24e","storageBucket":"da-bubble-8cc28.appspot.com","apiKey":"AIzaSyATkchfPwpyL6G3Y4_K1awoeA1M52agBmk","authDomain":"da-bubble-8cc28.firebaseapp.com","messagingSenderId":"380502943179"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideDatabase(() => getDatabase())]
 };
--- src/app/main/menu/menu.component.ts
+++ src/app/main/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-menu',
@@ -8,5 +9,45 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  constructor(public db: Firestore){}
 
+  userData:any[] = [];
+
+  async ngOnInit(){
+    await this.getAllUser('users');
+  }
+
+  async getAllUser(users: string) {
+    try {
+      const usersCollectionRef = collection(this.db, users);
+      this.getUsersDataOnSnapshot(usersCollectionRef);
+      console.log(this.userData)
+    } catch (error) {
+      console.error('Fehler beim Aktualisieren der Daten:', error);
+    }
+  }
+  
+  getUsersDataOnSnapshot(usersCollectionRef: any) {
+    onSnapshot(
+      usersCollectionRef,
+      (snapshot: { docs: any[] }) => {
+        this.userData = [];
+        this.userData = snapshot.docs.map((doc) => {
+          const user = doc.data();
+      
+          return {
+            userID: doc.id,
+            name: user['name'],
+            avatar: user['avatar'],
+            mail: user['mail'],
+            online: user['online'],
+          };
+          
+        });
+      },
+      (error) => {
+        console.error('Fehler beim laden der User-Daten:', error);
+      }
+    );
+  }
 }
