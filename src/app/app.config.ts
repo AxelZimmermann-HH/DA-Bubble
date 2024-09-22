8e1ce5bcf07d8658eb798e517f74517e91cef4e8
Oliver <oliver.azzolina@gmx.de>
2024-09-22 20:29:51 +0200

    changed menu width

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -20,5 +20,5 @@ export const appConfig: ApplicationConfig = {
     "messagingSenderId":"380502943179"})), 
     provideAuth(() => getAuth()), 
     provideFirestore(() => getFirestore()), 
-    provideAnimationsAsync()]
+    provideAnimationsAsync(), provideAnimationsAsync()]
 };
