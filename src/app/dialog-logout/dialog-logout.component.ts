4e7059ce25d4ab45c1c0322a644a3c7a6c4a0a25
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-09-24 17:24:25 +0200

    logout dialog

---
+++ src/app/dialog-logout/dialog-logout.component.ts
@@ -0,0 +1,34 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Channel } from '../models/channel.class';
+import { User } from '../models/user.class';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './dialog-logout.component.html',
+  styleUrl: './dialog-logout.component.scss'
+})
+export class DialogLogoutComponent {
+  channel = new Channel();
+  channelData: any = [];
+  channelId!: string;
+  userData: any = [];
+  user = new User();
+  selectedOption: string | null = null;
+
+  constructor(public firestore: Firestore, public dialogRef: MatDialogRef<DialogLogoutComponent>) {
+    //provisorisch
+    this.channel.channelName = "Entwicklerteam";
+    this.user.name = "Noah";
+  }
+  addUser() {
+    console.log('user erstellt');
+    this.dialogRef.close();
+  }
+
+}
--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
-import { SharedService } from '../../services/shared.service'; // Pfad zum Service anpassen
+import { SharedService } from '../../services/shared.service';
+import { DialogAddUserComponent } from '../../dialog-add-user/dialog-add-user.component';
+import { DialogLogoutComponent } from '../../dialog-logout/dialog-logout.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private sharedService: SharedService) {}
+  constructor(public dialog: MatDialog, private sharedService: SharedService) {}
 
   onSearchInput(event: any) {
     const searchTerm = event.target.value;
@@ -20,4 +28,10 @@ export class HeaderComponent {
       this.sharedService.updateSearchTerm('');
     }
   }
+
+  openDialogLogout() {
+    this.dialog.open(DialogLogoutComponent)
+  }
 }
+
+
