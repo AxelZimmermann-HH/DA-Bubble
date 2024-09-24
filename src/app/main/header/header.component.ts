abbfd71c76d84ab4f1ad3e81cda1e21813f73345
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-09-24 16:42:10 +0200

    search function started

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SharedService } from '../../services/shared.service'; // Pfad zum Service anpassen
 
 @Component({
   selector: 'app-header',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  constructor(private sharedService: SharedService) {}
 
+  onSearchInput(event: any) {
+    const searchTerm = event.target.value;
+  
+    if (searchTerm.length >= 3) {
+      this.sharedService.updateSearchTerm(searchTerm);
+    } else {
+      this.sharedService.updateSearchTerm('');
+    }
+  }
 }
