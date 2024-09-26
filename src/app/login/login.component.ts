074a04246e2627f108ce499b5b7b3bc84b878d4b
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-09-26 13:03:29 +0200

    user-class updated, added pw

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
 import { User } from '../models/user.class';
@@ -13,9 +13,12 @@ import { User } from '../models/user.class';
 })
 export class LoginComponent {
 
+  @ViewChild('mailInput') mailInput!: ElementRef;
   userData: any = [];
   user = new User();
   userCount: number = 0;
+  validMail: boolean = true;
+  validPassword: boolean = true;
 
   constructor(public firestore: Firestore) {
     this.getAllUsers();
@@ -35,17 +38,43 @@ export class LoginComponent {
   }
 
   onSubmit(ngForm: NgForm) {
-    const enteredMail = ngForm.value.mailInput;
-    
-    const userExists = this.userData.some((user: User) => user.mail === enteredMail);
-    
-    
-    if (userExists) {
-      console.log('Login successful for:', enteredMail);
-      // Hier könntest du weitere Logik einfügen, z.B. Navigation oder Session-Management
+    const enteredMail = this.user.mail;
+    const enteredPassword = this.user.password;
+  
+    const user = this.userData.find((user: User) => user.mail === enteredMail);
+  
+    // Leere in jedem Fall beide Felder
+    this.user.mail = '';  // Leeren des Mail-Feldes
+    this.user.password = '';  // Leeren des Passwort-Feldes
+  
+    // Initialisiere beide als ungültig
+    this.validMail = true;
+    this.validPassword = true;
+  
+    if (user) {
+      // Wenn die E-Mail korrekt ist, überprüfe das Passwort
+      if (user.password === enteredPassword) {
+        console.log('Login successful for:', enteredMail);
+        this.validMail = true;
+        this.validPassword = true;
+        // Hier kann der tatsächliche submit stattfinden
+      } else {
+        console.log('Falsches Passwort');
+        this.validMail = true;  // E-Mail korrekt
+        this.validPassword = false;  // Passwort falsch
+      }
     } else {
       console.log('E-Mail nicht gefunden');
+      this.validMail = false;  // E-Mail falsch
+      this.validPassword = true;  // Passwort-Fehler wird zurückgesetzt
     }
+  
+    // Blockiere den erfolgreichen Submit, wenn eines der Felder falsch ist
+    if (!this.validMail || !this.validPassword) {
+      return;  // Verhindere den weiteren Ablauf, wenn eines ungültig ist
+    }
+  
+    // Weiterführende Logik bei erfolgreichem Submit
   }
 
 }
