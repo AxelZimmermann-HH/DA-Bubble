5a362921f9b4462f7e3ae7e26b7df92ce694f1f0
Oliver <oliver.azzolina@gmx.de>
2024-09-27 22:44:29 +0200

    added send direct message, save it in firebase, css updatet

--- src/app/main/chat/chat.component.ts
+++ src/app/main/chat/chat.component.ts
@@ -1,22 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ChatService } from '../../services/chat.service';
 import { CommonModule } from '@angular/common';
+import { FormControl, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './chat.component.html',
   styleUrl: './chat.component.scss'
 })
 
 export class ChatComponent {
 
-  
+  directMessage = new FormControl('', [Validators.required, Validators.minLength(2)])
   myUser:any;
   user:any;
   chat:any;
  
+  @ViewChild('chatContainer') private chatContainer!: ElementRef;
 
   constructor(public chatService: ChatService){}
 
@@ -45,18 +47,31 @@ export class ChatComponent {
         this.chat = chatSubject;
         this.chatService.chatMessages.push(chatSubject)
       }
-
+  
       this.chatService.chatMessages.sort((a, b) => new Date(a.timestamp).getTime() - new Date(b.timestamp).getTime());
-      
-      //console.log(this.chatService.chatMessages)
     });
   }
 
-  
+  async sendDirectMessage(){
+    const newDm = this.directMessage.value!;
+    await this.chatService.setChatData(newDm)
+    this.directMessage.setValue('');
+  }
 
+  ngAfterViewChecked() {        
+    this.scrollToBottom();        
+  }
 
-//ANZEIGE WENN MESSAGES VORHANDEN
-//ANZEIGE WENN KEINE MESSAGES VORHANDEN
+  scrollToBottom(): void {
+    if(!this.chatService.chatIsEmpty){
+      try {
+        this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+      } catch (err) {
+        console.error('Scrollen fehlgeschlagen:', err);
+      }
+    }
+
+  }
 
   openUserProfile(){
 
