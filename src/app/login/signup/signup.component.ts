b02bec0afb9cc71f1ae046377ed6be2e77caa741
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-09-29 14:42:04 +0200

    signup continued

--- src/app/login/signup/signup.component.ts
+++ src/app/login/signup/signup.component.ts
@@ -1,13 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FirstpageComponent } from './firstpage/firstpage.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FirstpageComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent {
-
+  firstPage: boolean = true;
 }
