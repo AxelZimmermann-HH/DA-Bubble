02ef0446634764f226a36c3398efebaa436b7a0d
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-11-15 09:38:59 +0100

    dsgvo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,15 @@ import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { ImprintComponent } from './login/imprint/imprint.component';
+import { PrivacyPolicyComponent } from './login/privacy-policy/privacy-policy.component';
+
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent, },
     { path: 'login/:userId', component: MainComponent },
     { path: 'imprint', component: ImprintComponent },
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
     { path: 'reset', component: PasswordResetComponent }
 ];
