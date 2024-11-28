faeb98ba88e2a8d1b2d00a74fa8ac6e3d68b540d
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-11-28 15:29:56 +0100

    started change mail

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,14 +4,18 @@ import { LoginComponent } from './login/login.component';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { ImprintComponent } from './login/imprint/imprint.component';
 import { PrivacyPolicyComponent } from './login/privacy-policy/privacy-policy.component';
+import { MailChangedComponent } from './mail-changed/mail-changed.component';
+import { HandlerComponent } from './handler/handler.component';
 
 
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent, },
     { path: 'login/:userId', component: MainComponent },
+    { path: 'handler', component: HandlerComponent },
     { path: 'imprint', component: ImprintComponent },
     { path: 'privacy-policy', component: PrivacyPolicyComponent },
     { path: '', redirectTo: 'login', pathMatch: 'full' },
-    { path: 'reset', component: PasswordResetComponent }
+    { path: 'reset', component: PasswordResetComponent },
+    { path: 'mail-changed', component: MailChangedComponent }
 ];
