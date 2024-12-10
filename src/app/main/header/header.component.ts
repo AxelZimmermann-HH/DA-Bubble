6731a3d997c3aac638e97341874f7003f2fe3784
Axel Zimmermann <168816379+AxelZimmermann-HH@users.noreply.github.com>
2024-12-10 14:54:06 +0100

    direct user search

--- src/app/main/header/header.component.ts
+++ src/app/main/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostListener } from '@angular/core';
+import { Component, OnInit, HostListener, EventEmitter, Output } from '@angular/core';
 import { SharedService } from '../../services/shared.service';
 import { DialogLogoutComponent } from './dialog-logout/dialog-logout.component';
 import { CommonModule } from '@angular/common';
@@ -27,6 +27,10 @@ export class HeaderComponent implements OnInit {
   currentUser: User | null = null;
   userData: any = [];
   userId!: string;
+  showUsers: boolean = false;
+  userList: User[] = [];
+  filteredUserList: User[] = [];
+  searchTerm: string = '';
 
   constructor(
     public dialog: MatDialog, 
@@ -54,7 +58,8 @@ export class HeaderComponent implements OnInit {
       this.getUserById(this.userId);
     });
 
-    this.getAllUsers(); 
+    this.getAllUsers();
+    this.filteredUserList = [];
   }
 
   getAllUsers() {
@@ -80,6 +85,74 @@ export class HeaderComponent implements OnInit {
     }).catch(error => {
       console.error('Fehler beim Abrufen des Benutzers:', error);
     });
+  }
+
+  handleInput(event: any) {
+    const searchTerm = event.target.value;
+  
+    if (searchTerm.startsWith('@')) {
+      const query = searchTerm.slice(1); 
+      this.filterUsers(query); 
+      this.toggleUserList();
+    } else if (searchTerm.startsWith('#')) { 
+      this.test2();
+    } else {
+      this.onSearchInput(event);} 
+
+    if (!searchTerm.includes('@')) {
+      this.showUsers = false;
+    }
+  }
+
+  toggleUserList() {
+    if (this.userList.length === 0) {
+      this.userService.loadUsers()
+        .then(() => {
+          this.userList = this.userService.userData;
+          this.filteredUserList = [...this.userList];        
+          this.showUsers = true; 
+        })
+        .catch(error => {
+          console.error('Fehler beim Laden der Benutzer:', error);
+        });
+    } else {
+      this.showUsers = true;
+    }
+  }
+
+  filterUsers(searchTerm: string) {
+    const searchQuery = searchTerm.toLowerCase(); 
+    this.filteredUserList = this.userList.filter(user =>
+      user.name.toLowerCase().includes(searchQuery)
+    );
+  }
+
+  changeToUser(user: User) {
+    if (!this.currentUser?.userId) {
+      console.error('Fehler: Der aktuelle Benutzer ist nicht definiert.');
+      return;
+    }
+
+    this.userService.loadCurrentUser(this.currentUser.userId)
+
+    this.showUsers = false;
+    this.searchTerm = '';
+    // Chat mit dem Benutzer öffnen
+    const currentUserId = this.currentUser.userId; // Aktueller Benutzer
+    const userId = user.userId; // Ausgewählter Benutzer
+
+    this.chatService.openDirectMessage(currentUserId, userId)
+      .then(() => {
+        console.log(`Chat mit ${user.name} erfolgreich geöffnet.`);
+        this.chatService.onChatSelected();
+      })
+      .catch(error => {
+        console.error('Fehler beim Öffnen des Chats:', error);
+      });
+  }
+
+  test2() {
+    console.log('RAUTE');
   }
 
   onSearchInput(event: any) {
@@ -129,3 +202,4 @@ export class HeaderComponent implements OnInit {
       : avatar;
   }
 }
+
