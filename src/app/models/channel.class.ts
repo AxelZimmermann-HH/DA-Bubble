bc5e3ede060bd8260c1eb54afaf07c5642c68e99
Takoua Jelassi <takoua.jelassi@aislab.de>
2024-12-17 18:12:10 +0100

    thread css

--- src/app/models/channel.class.ts
+++ src/app/models/channel.class.ts
@@ -2,29 +2,30 @@ import { User } from "./user.class";
 
 export class Channel {
     channelName!: string;
-    channelDescription!:string;
+    channelDescription!: string;
     id!: string;
     tagIcon!: string;
     members!: User[];
-    creatorName!: string; 
-    creator: User|undefined; 
+    creatorName!: string;
+    creator: User | undefined;
+    creatorId!: User;
 
     constructor(obj?: any) {
         this.channelName = obj ? obj.channelName : '';
         this.channelDescription = obj ? obj.channelDescription : '';
         this.id = obj ? obj.id : '';
         this.tagIcon = obj ? obj.tagIcon : '';
-        this.members = Array.isArray(obj?.members) ? obj.members : []; 
+        this.members = Array.isArray(obj?.members) ? obj.members : [];
         this.creatorName = obj ? obj.creatorName : '';
         this.creator = obj?.creator ? new User(obj.creator) : new User(); // Vollständige Benutzerdaten
     }
-  
+
     public toJson() {
         return {
-            channelName:this.channelName,
+            channelName: this.channelName,
             channelDescription: this.channelDescription,
-            id:this.id,
-            tagIcon:this.tagIcon,
+            id: this.id,
+            tagIcon: this.tagIcon,
             members: this.members,
             creatorName: this.creatorName,
             creator: this.creator?.toJson()// Vollständige Benutzerdaten
