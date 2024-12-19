afe0fcad38f9ee4d285fe88f1edf450434d3821a
Oliver <oliver.azzolina@gmx.de>
2024-12-19 16:39:12 +0100

    debugger deleted

--- src/app/main/menu/menu.component.ts
+++ src/app/main/menu/menu.component.ts
@@ -260,7 +260,6 @@ export class MenuComponent {
 
   //öffnet den Channel hinzufügen Dialog
   openDialogAddChannel() {
-    debugger
     const isMobile = this.breakpointObserver.isMatched('(max-width: 600px)');
     const dialogConfig = {
       width: isMobile ? '100vw' : '600px',
